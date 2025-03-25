defb9572d9c2272a193e1abc4ec97be65be339e4
freezing1116 <freezing16@naver.com>
2025-03-25 15:41:24 +0900

    thread_unblock(), thread_yield() modified

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -245,7 +245,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_insert_ordered (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem, cmp_priority_desc, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -316,7 +316,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, cmp_priority_desc, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -586,7 +586,7 @@ allocate_tid (void)
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
 
-// * Newly added functions
+//* Newly added functions
 bool cmp_priority_desc(struct list_elem *a, struct list_elem *b, void *aux UNUSED) {
   return list_entry(a, struct thread, elem)->priority > list_entry(b, struct thread, elem)->priority;
 }
