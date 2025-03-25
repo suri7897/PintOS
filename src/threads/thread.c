ea0f84953d4b5e2f005ea7e64688e9f8218905f4
freezing1116 <freezing16@naver.com>
2025-03-25 15:25:37 +0900

    update cmp_priority_desc in thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -245,7 +245,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -585,3 +585,8 @@ allocate_tid (void)
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
+
+// * Newly added functions
+bool cmp_priority_desc(struct list_elem *a, struct list_elem *b, void *aux UNUSED) {
+  return list_entry(a, struct thread, elem)->priority > list_entry(b, struct thread, elem)->priority;
+}
