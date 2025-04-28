cdaf17b889a81c55a1cb4fbf1a5f1e6fa9e8704b
이강준 <suri7897@igangjun-ui-noteubug.local>
2025-04-28 18:24:52 +0900

    Write added

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,7 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include "userprog/pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -17,8 +18,8 @@ syscall_init (void)
 
 void exit(int status){
   struct thread *cur = thread_current();
-  printf("%s: exit(%d)\n", cur->name, status);
-  cur->exit_status = status;
+  printf("%s: exit(%d)\n", cur->name, status); //! exit call
+  cur->exit_status = status; //! save exit status
   thread_exit(); 
 }
 
@@ -30,14 +31,32 @@ void halt (void)
 
 void is_valid_addr(void *addr)
 {
-  if(!is_user_vaddr(addr) || 
-    pagedir_get_page(thread_current()->pagedir, addr) == NULL)
+  if(!is_user_vaddr(addr) ||  //! is address user address
+    pagedir_get_page(thread_current()->pagedir, addr) == NULL) //! check whether address is in page table.
   {
     exit(-1);
   }
 }
 
+int write(int fd, void *buffer, unsigned size){
+  struct thread *cur = thread_current();
+  if(fd == 1){ //! if fd == 1, then put text in buffer.
+    putbuf(buffer, size);
+  }
+  return -1;
+}
 
+bool create(const char*file, unsigned initial_size){
+  return true;
+} //! not implemented yet
+
+int open(const char *file){
+  return 1;
+} //! not implemented yet
+
+void close(int fd){
+  return;
+} //! not implemented yet
 
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
@@ -48,12 +67,12 @@ syscall_handler (struct intr_frame *f UNUSED)
   switch(*(uint32_t *)(f->esp))
   {
     case SYS_HALT:
-      halt();
+      halt(); 
       break;
     
     case SYS_EXIT:
-      is_valid_addr(f->esp+4);
-      exit(*(int*)(f->esp+4));
+      is_valid_addr(f->esp+4); //! check whether exit status is valid.
+      exit(*(int*)(f->esp+4)); //! do exit.
       break;
 
     case SYS_CREATE:
@@ -65,8 +84,14 @@ syscall_handler (struct intr_frame *f UNUSED)
     case SYS_CLOSE:
       break; //! not implemented yet
 
-    case SYS_WRITE:
-      break; //! not implemented yet
+    case SYS_WRITE: 
+    //! ex) write(1, "hello", 5); 
+    //! -> esp : SYS_WRITE, esp + 4 : (fd) = 1, esp + 8 : buffer_pointer (pointer of "hello"), esp + 12 : buffer_size (5)
+      is_valid_addr(f->esp + 4); //! check fd is valid
+      is_valid_addr(f->esp + 8); //! check buffer_pointer is valid
+      is_valid_addr(f->esp + 12); //! check buffer_size is valid
+      f->eax = write((int)*(uint32_t *)(f->esp+4), (void *)*(uint32_t *)(f->esp + 8), (unsigned)*((uint32_t *)(f->esp + 12))); //! store system call return value.
+      break;
 
     default:
       printf("system call!\n");
